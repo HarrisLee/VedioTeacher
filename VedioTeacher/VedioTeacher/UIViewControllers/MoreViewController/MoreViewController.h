fc3d877681bda38b0a7b27c67327cb84e9c23596
HarrisKent <skyshow520@vip.qq.com>
2014-07-07 17:16:39 +0800

    One Level Class Additional
    One Level Class Additional

---
+++ VedioTeacher/VedioTeacher/UIViewControllers/MoreViewController/MoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreViewController.h
+//  VedioTeacher
+//
+//  Created by Cao JianRong on 14-7-7.
+//  Copyright (c) 2014年 Cao JianRong. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MoreViewController : BaseViewController
+
+@end
